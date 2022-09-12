bff758fe6224692fede4c85d4a6a144b4268ceb9
mdasif8393 <md.asif.8393@gmail.com>
2022-09-12 12:54:25 +0600

    first

---
+++ __steps.js
@@ -0,0 +1,9 @@
+/**
+ * 1. Create a folder
+ * 2. Open command line to that folder
+ * 3. npm init
+ * -------------------------------
+ * For Express
+ * 4. npm install express
+ * -------------------------------
+ */
